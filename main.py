02f0fff93c8c4b6486686cd7fff83c1d486fdba3
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-08-12 22:09:18 +0500

    Create super_admin.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from main_files.auth import Auth
 from main_files.decorator_func import log_decorator
 
 
+# This function is auth menu
 @log_decorator
 def show_auth():
     text = '''
@@ -30,6 +31,70 @@ def show_auth():
         show_auth()
 
 
+# this function for super admin
+@log_decorator
+def show_super_admin_menu():
+    text = '''
+1. Admin
+2. Student 
+3. Send message
+4. Logout
+    '''
+    print(text)
+    try:
+        user_input = int(input('Choose menu: '))
+        if user_input == 1:
+            pass
+        elif user_input == 2:
+            admin_student_menu()
+        elif user_input == 3:
+            pass
+        elif user_input == 4:
+            auth.logout()
+            show_auth()
+        else:
+            print('Wrong input')
+            show_super_admin_menu()
+    except ValueError:
+        print("Invalid input")
+        show_super_admin_menu()
+    except Exception as e:
+        print(f'Error: {e}')
+        show_super_admin_menu()
+
+
+# student section belonging to super admin
+@log_decorator
+def super_admin_student_menu():
+    text = '''
+1. Create student
+2. Update student
+3. Delete student
+4. Show all students
+5. Back
+    '''
+    print(text)
+    try:
+        user_input = int(input('Choose menu: '))
+        if user_input == 1:
+            pass
+        elif user_input == 2:
+            pass
+        elif user_input == 3:
+            pass
+        elif user_input == 4:
+            pass
+        elif user_input == 5:
+            show_super_admin_menu()
+    except ValueError:
+        print("Invalid input")
+        super_admin_student_menu()
+    except Exception as e:
+        print(f'Error: {e}')
+        super_admin_student_menu()
+
+
+# This section is for regular admins
 @log_decorator
 def show_admin_menu():
     text = '''
@@ -58,6 +123,7 @@ def show_admin_menu():
         show_admin_menu()
 
 
+# A function related to groups in the Admin menu
 @log_decorator
 def admin_group_menu():
     text = '''
@@ -88,6 +154,7 @@ def admin_group_menu():
         admin_group_menu()
 
 
+# A function related to groups in the Student menu
 @log_decorator
 def admin_student_menu():
     text = '''
@@ -118,6 +185,7 @@ def admin_student_menu():
         admin_student_menu()
 
 
+# A function for teachers
 @log_decorator
 def show_teacher_menu():
     text = '''
@@ -146,6 +214,7 @@ def show_teacher_menu():
         show_teacher_menu()
 
 
+# A function for student
 @log_decorator
 def show_student_menu():
     text = '''
@@ -174,6 +243,7 @@ def show_student_menu():
         show_student_menu()
 
 
+# run program
 if __name__ == '__main__':
     auth = Auth()
     auth.logout()
